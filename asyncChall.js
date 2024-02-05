9c82d08522deeb25ded4f3f634d0e035b281f844
daulat <daulatjajra2006@gmail.com>
2024-02-05 10:55:02 +0530

    async/await

--- asyncChall.js
+++ asyncChall.js
@@ -1,4 +1,28 @@
-// let renderCountry = function (data) {
+// let renderCountry = function (data, className = "") {
+//   let html = `
+//      <article class="country ${className}">
+//           <img class="country__img" src="${data.flags?.png}" />
+//           <div class="country__data">
+//             <h3 class="country__name">${data.name.common}</h3>
+//             <h4 class="country__region">${data.region}</h4>
+//             <p class="country__row"><span>👫</span>${(
+//               +data.population / 10000000
+//             ).toFixed(1)}</p>
+//          <p class="country__row"><span>🗣️</span>${Object.values(
+//            data.languages
+//          ).join(", ")}</p>
+//         <p class="country__row"><span>💰</span>${
+//           Object.values(data.currencies)[0]?.name
+//         }</p>
+
+//           </div>
+//         </article>
+//     `;
+//   countriesContainer.insertAdjacentHTML("beforeend", html);
+//   countriesContainer.style.opacity = 1;
+// };
+
+// let renderCity = function (data) {
 //   let html = `
 //      <article class="country">
 //          <p class="country__row"><span>Country :  </span>${data.address.country}</p>
@@ -13,25 +37,205 @@
 // };
 
 // let whereAmI = function (lat, lng) {
-//   let response = fetch(
+//   fetch(
 //     ` https://geocode.maps.co/reverse?lat=${lat}&lon=${lng}&api_key=65bb472564d41430332438yuf71185a`
 //   )
-//     .then(function (response) {
-//       console.log(response);
-//       if (!response.ok)
-//         throw new Error(`Problem with geocoding ${response.status}`);
+//     .then((res) => {
+//       if (!res.ok) {
+//         throw new Error(`Problem with geocoding ${res.status}`);
+//       }
+//       return res.json();
+//     })
+//     .then((data) => {
+//       console.log(data);
+//       renderCity(data);
+//       console.log(`You are in ${data.address.city}, ${data.address.country}`);
 
+//       return fetch(
+//         `https://restcountries.com/v3.1/name/${data.address.country}`
+//         // `https://restcountries.com/v3.1/name/bharat`
+//       );
+//     })
+//     .then((response) => {
+//       if (!response.ok) {
+//         throw new Error(`Country not found (${response.status})`);
+//       }
 //       return response.json();
 //     })
-//     .then(function (data) {
-//       console.log(data);
-//       console.log(`You are in ${data.address.city}, ${data.address.country}`);
-//       renderCountry(data);
+//     .then((data) => {
+//       console.log(data[1]);
+//       renderCountry(data[1]);
 //     })
-//     .catch(function (error) {
-//       console.log(error);
-//     });
+//     .catch((err) => console.log(err));
 // };
 // // whereAmI(52.508, 13.381);
 // whereAmI(19.037, 72.873);
 // // whereAmI(-33.933, 18.474);
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+let renderCountry = function (data, className = "") {
+  let html = `
+     <article class="country ${className}">
+        <img class="country__img" src="${data.flags.png}" />
+          <div class="country__data">
+            <h3 class="country__name">${data.name?.common}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(
+              +data.population / 10000000
+            ).toFixed(1)}</p>
+         <p class="country__row"><span>🗣️</span>${Object.values(
+           data.languages
+         ).join(", ")}</p>
+        <p class="country__row"><span>💰</span>${
+          Object.values(data.currencies)[0]?.name
+        }</p>
+
+          </div>
+        </article>
+    `;
+  countriesContainer.insertAdjacentHTML("beforeend", html);
+  countriesContainer.style.opacity = 1;
+};
+
+let renderCity = function (data) {
+  let html = `
+     <article class="country">
+         <p class="country__row"><span>Country :  </span>${data.address.country}</p>
+         <p class="country__row"><span>State :  </span>${data.address.state}</p>
+         <p class="country__row"><span>City :  </span>${data.address.city}</p>
+         <p class="country__row"><span>Post code : </span>${data.address.postcode}</p>
+         <p class="country__row"><span>Address : </span>${data.display_name}</p>
+        </article>
+    `;
+  countriesContainer.insertAdjacentHTML("beforeend", html);
+  countriesContainer.style.opacity = 1;
+};
+
+// let getPosition = function () {
+//   return new Promise((resolve, reject) => {
+//     // navigator.geolocation.getCurrentPosition(
+//     //   (pos) => resolve(pos),
+//     //   (err) => reject(err)
+//     // );
+//     navigator.geolocation.getCurrentPosition(resolve, reject);
+//   });
+// };
+
+// getPosition().then((pos) => console.log(pos));
+
+// let whereAmI = function () {
+//   getPosition()
+//     .then((pos) => {
+//       let { latitude: lat, longitude: lng } = pos.coords;
+//       console.log(lat, lng);
+//       console.log(pos);
+
+//       return fetch(
+//         ` https://geocode.maps.co/reverse?lat=${lat}&lon=${lng}&api_key=65bb472564d41430332438yuf71185a`
+//       );
+//     })
+//     .then((res) => {
+//       if (!res.ok) {
+//         throw new Error(`Problem with geocoding ${res.status}`);
+//       }
+//       return res.json();
+//     })
+//     .then((data) => {
+//       console.log(data);
+//       renderCity(data);
+//       console.log(`You are in ${data.address.city}, ${data.address.country}`);
+
+//       return fetch(
+//         `https://restcountries.com/v3.1/name/${data.address.country}`
+//         // `https://restcountries.com/v3.1/name/bharat`
+//       );
+//     })
+//     .then((response) => {
+//       if (!response.ok) {
+//         throw new Error(`Country not found (${response.status})`);
+//       }
+//       return response.json();
+//     })
+//     .then((data) => {
+//       console.log(data[1]);
+//       renderCountry(data[1]);
+//     })
+//     .catch((err) => console.log(err));
+// };
+// // whereAmI(52.508, 13.381);
+// whereAmI(19.037, 72.873);
+// // whereAmI(-33.933, 18.474);
+// btn.addEventListener("click", whereAmI);
+
+///////////////////////////////////////////////////////////////////////////////////////
+
+// Async/Await
+
+let getPosition = function () {
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+};
+let renderError = function (err) {
+  countriesContainer.insertAdjacentText("beforeend", err);
+  countriesContainer.style.opacity = 1;
+};
+
+// getPosition().then((pos) => console.log(pos));
+
+let whereAmI = async function () {
+  try {
+    let pos = await getPosition();
+    let { latitude: lat, longitude: lng } = pos.coords;
+
+    let resGeo = await fetch(
+      ` https://geocode.maps.co/reverse?lat=${lat}&lon=${lng}&api_key=65bb472564d41430332438yuf71185a`
+    );
+    if (!resGeo.ok) {
+      throw new Error("Problem getting location data");
+    }
+    let dataGeo = await resGeo.json();
+    // console.log(dataGeo);
+
+    let res = await fetch(
+      `https://restcountries.com/v3.1/name/${dataGeo.address.country}`
+    );
+    let data = await res.json();
+    // console.log(data);
+    renderCountry(data[1]);
+
+    return `You are in ${dataGeo.address.city}, ${dataGeo.address.country}`;
+  } catch (err) {
+    console.error(err);
+    renderError(`Something went wrong  ${err.message}`);
+
+    //Reject promise returned from async function
+    throw err;
+  }
+};
+//////////////////////////////////////////////////////////////////////////////////////////
+
+//// Returning data from Async/Await
+
+// console.log("1: Start Getting location");
+// let where = whereAmI();
+// console.log(where);
+
+// whereAmI()
+//   .then((city) => console.log`2: ${city}`)
+//   .catch((err) => console.error(`2: ${err.message}`))
+//   .finally(() => console.log("3: Finished getting location"));
+// console.log("3  : Finish Getting location");
+
+// (async function () {
+//   try {
+//     let city = await whereAmI();
+//     console.log(`2: ${city}`);
+//   } catch (err) {
+//     console.error(`2: ${err.message}`);
+//   }
+//   console.log(`3: Finished getting location`);
+// })();
+
+///////////////////////////////////////////////////////////////////////////////////////////////
--- script.js
+++ script.js
@@ -318,57 +318,209 @@ const countriesContainer = document.querySelector(".countries");
 
 // console.log("Test End ");
 
-///////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////
 
 //// Build a simple promise
 
-let lotteryPromise = new Promise(function (resolve, reject) {
-  console.log("Lottery draw is happening");
-  setTimeout(() => {
-    Math.random() >= 0.5 ? resolve("You won!") : reject(new Error("You lost!"));
-  }, 4000);
-});
+// let lotteryPromise = new Promise(function (resolve, reject) {
+//   console.log("Lottery draw is happening");
+//   setTimeout(() => {
+//     Math.random() >= 0.5 ? resolve("You won!") : reject(new Error("You lost!"));
+//   }, 4000);
+// });
 
-lotteryPromise
-  .then((res) => console.log(res))
-  .catch((err) => console.error(err));
+// lotteryPromise
+//   .then((res) => console.log(res))
+//   .catch((err) => console.error(err));
 
-// Promisifying setTimeout
-let wait = function (seconds) {
-  return new Promise(function (resolve) {
-    setTimeout(resolve, seconds * 1000);
+// // Promisifying setTimeout
+// let wait = function (seconds) {
+//   return new Promise(function (resolve) {
+//     setTimeout(resolve, seconds * 1000);
+//   });
+// };
+
+// wait(2)
+//   .then(() => {
+//     console.log("1 second passed");
+//     return wait(1);
+//   })
+//   .then(() => {
+//     console.log("2 seconds passed");
+//     return wait(1);
+//   })
+//   .then(() => {
+//     console.log("3 seconds passed");
+//     return wait(1);
+//   })
+//   .then(() => {
+//     console.log("4 seconds passed");
+//     return wait(1);
+//   })
+//   .then(() => {
+//     console.log("5 seconds passed");
+//     return wait(1);
+//   })
+//   .then(() => {
+//     console.log("6 seconds passed");
+//     return wait(1);
+//   })
+//   .then(() => {
+//     console.log("7 seconds passed");
+//   });
+
+// //
+// Promise.resolve("It resolve Immediately").then((x) => console.log(x));
+// Promise.reject(new Error("Problem!")).catch((x) => console.log(x));
+
+///////////////////////////////////////////////////////
+
+///// Promisifying the Geolocation API
+
+// let getPosition = function () {
+//   return new Promise((resolve, reject) => {
+//     // navigator.geolocation.getCurrentPosition(
+//     //   (pos) => resolve(pos),
+//     //   (err) => reject(err)
+//     // );
+//     navigator.geolocation.getCurrentPosition(resolve, reject);
+//   });
+// };
+
+// getPosition().then((pos) => console.log(pos));
+////////////////////////////////////////////////////////////////////////////////
+
+//// Async / Await :
+
+// let WhereAmI = async function (country) {
+//   // Geolocation
+//   let pos = await
+//   ///
+//   ///
+//   //
+//   //
+//   //
+//   let res = await fetch(`https://restcountries.eu/rest/v2/name/${country}`);
+
+//   let data = res.json();
+//   console.log(data);
+//   renderCountry(data[0]);
+// };
+// WhereAmI("Bharat");
+// console.log("First");
+
+//////////////////////////////////////////////////////////
+
+//// Error Handling with TRY and CATCH
+
+// try {
+//   let x = 1;
+//   const y = 2;
+//   y = 4;
+//   console.log(y);
+// } catch (err) {
+//   alert(err);
+// }
+///////////////////////////////////////////////////////////////////////////////////////
+
+// Async/Await
+
+// let getPosition = function () {
+//   return new Promise((resolve, reject) => {
+//     navigator.geolocation.getCurrentPosition(resolve, reject);
+//   });
+// };
+// let renderError = function (err) {
+//   countriesContainer.insertAdjacentText("beforeend", err);
+//   countriesContainer.style.opacity = 1;
+// };
+
+// // getPosition().then((pos) => console.log(pos));
+
+// let whereAmI = async function () {
+//   try {
+//     let pos = await getPosition();
+//     let { latitude: lat, longitude: lng } = pos.coords;
+
+//     let resGeo = await fetch(
+//       ` https://geocode.maps.co/reverse?lat=${lat}&lon=${lng}&api_key=65bb472564d41430332438yuf71185a`
+//     );
+//     if (!resGeo.ok) {
+//       throw new Error("Problem getting location data");
+//     }
+//     let dataGeo = await resGeo.json();
+//     // console.log(dataGeo);
+
+//     let res = await fetch(
+//       `https://restcountries.com/v3.1/name/${dataGeo.address.country}`
+//     );
+//     let data = await res.json();
+//     // console.log(data);
+//     renderCountry(data[1]);
+
+//     return `You are in ${dataGeo.address.city}, ${dataGeo.address.country}`;
+//   } catch (err) {
+//     console.error(err);
+//     renderError(`Something went wrong  ${err.message}`);
+
+//     //Reject promise returned from async function
+//     throw err;
+//   }
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//// Returning data from Async/Await
+
+// console.log("1: Start Getting location");
+// let where = whereAmI();
+// console.log(where);
+
+// whereAmI()
+//   .then((city) => console.log`2: ${city}`)
+//   .catch((err) => console.error(`2: ${err.message}`))
+//   .finally(() => console.log("3: Finished getting location"));
+// console.log("3  : Finish Getting location");
+
+// (async function () {
+//   try {
+//     let city = await whereAmI();
+//     console.log(`2: ${city}`);
+//   } catch (err) {
+//     console.error(`2: ${err.message}`);
+//   }
+//   console.log(`3: Finished getting location`);
+// })();
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+///// Running Promises in Parallel : Promise.all()
+
+let getJSON = function (url, errorMsg = "Something went wrong") {
+  return fetch(url).then((response) => {
+    if (!response.ok) throw new Error(`${errorMsg} ${response.status}`);
+    return response.json();
   });
 };
 
-wait(2)
-  .then(() => {
-    console.log("1 second passed");
-    return wait(1);
-  })
-  .then(() => {
-    console.log("2 seconds passed");
-    return wait(1);
-  })
-  .then(() => {
-    console.log("3 seconds passed");
-    return wait(1);
-  })
-  .then(() => {
-    console.log("4 seconds passed");
-    return wait(1);
-  })
-  .then(() => {
-    console.log("5 seconds passed");
-    return wait(1);
-  })
-  .then(() => {
-    console.log("6 seconds passed");
-    return wait(1);
-  })
-  .then(() => {
-    console.log("7 seconds passed");
-  });
+let get3Countries = async function (c1, c2, c3) {
+  try {
+    // let [data1] = await getJSON(`https://restcountries.com/v3.1/name/${c1}`);
+    // let [data2] = await getJSON(`https://restcountries.com/v3.1/name/${c2}`);
+    // let [data3] = await getJSON(`https://restcountries.com/v3.1/name/${c3}`);
 
-//
-Promise.resolve("It resolve Immediately").then((x) => console.log(x));
-Promise.reject(new Error("Problem!")).catch((x) => console.log(x));
+    let data = await Promise.all([
+      getJSON(`https://restcountries.com/v3.1/name/${c1}`),
+      getJSON(`https://restcountries.com/v3.1/name/${c2}`),
+      getJSON(`https://restcountries.com/v3.1/name/${c3}`),
+    ]);
+    // console.log(data);
+    console.log(data.map((d) => d[0].capital).map((d) => d.join(", ")));
+    // console.log(data1.capital, data2.capital, data3.capital);
+    // console.log(data1, data2, data3);
+  } catch (err) {
+    console.error(err.message);
+  }
+};
+get3Countries("bharat", "USA", "canada");
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
